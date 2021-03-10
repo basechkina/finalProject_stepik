7fb8f3b7c2a81dc588445c28fa65e3e13df9c6e9
basechkina <olgabasin94@gmail.com>
2021-03-10 17:06:51 +0200

    Added LoginPage implementation

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,5 @@
 from .pages.main_page import MainPage
-
-
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
+from .pages.login_page import LoginPage
 
 
 def test_guest_should_see_login_link(browser):
@@ -13,3 +7,10 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
